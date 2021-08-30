d8568339df3909afc2c7d9e00fd4813ab7d5a834
Martijn Storck <martijn@storck.io>
2021-08-30 11:58:19 +0200

    Clean up P1 Telegram parsing

--- Dsmr/P1Telegram.cs
+++ Dsmr/P1Telegram.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using InvertedTomato.IO;
@@ -12,48 +14,22 @@ namespace P1Dash.Dsmr
         {
             Valid = IsValidMessage(message);
 
-            var versionMatch = Regex.Match(message, @"1-3:0\.2\.8\((\d+)\)");
-
-            if (versionMatch.Success)
+            // Extract all fields in the telegram by regex
+            // Note that this only captures the first value for each OBIS code currently
+            foreach (Match match in Regex.Matches(message, @"(\d+-\d+:\d+\.\d+\.\d+)\((.*?)\)"))
             {
-                DsmrVersion = int.Parse(versionMatch.Groups[1].Value);
-            }
-            
-            var timestampMatch = Regex.Match(message, @"0-0:1\.0\.0\((\d{12}).\)");
-
-            if (timestampMatch.Success)
-            {
-                Timestamp = DateTime.ParseExact(timestampMatch.Groups[1].Value, "yyMMddhhmmss", CultureInfo.InvariantCulture);
-            }
-            
-            var deliveredMatch = Regex.Match(message, @"1-0:1\.7\.0\(([\d\.]+)\*kW\)");
-
-            if (deliveredMatch.Success)
-            {
-                ElectricityDelivered = double.Parse(deliveredMatch.Groups[1].Value);
-            }
-            
-            var receivedMatch = Regex.Match(message, @"1-0:2\.7\.0\(([\d\.]+)\*kW\)");
-
-            if (receivedMatch.Success)
-            {
-                ElectricityReceived = double.Parse(receivedMatch.Groups[1].Value);
+                Fields[match.Groups[1].Value] = match.Groups[2].Value;
             }
         }
         
-        //1-3:0.2.8(50)
-        public int DsmrVersion { get; set; }
-        
-        // 0-0:1.0.0(210830110308S)
-        public DateTime Timestamp { get; set; }
-
-        //1-0:1.7.0(00.659*kW)
-        public double ElectricityDelivered { get; set; }
-        //1-0:2.7.0(00.000*kW)
-        public double ElectricityReceived { get; set; }
-        
         public bool Valid { get; set; }
+        public int DsmrVersion => int.Parse(Fields["1-3:0.2.8"]);
+        public DateTime Timestamp => DateTime.ParseExact(Fields["0-0:1.0.0"].Remove(12, 1), "yyMMddhhmmss", CultureInfo.InvariantCulture);
+        public double ElectricityDelivered => double.Parse(Fields["1-0:1.7.0"].Split("*").First());
+        public double ElectricityReceived => double.Parse(Fields["1-0:2.7.0"].Split("*").First());
 
+        private Dictionary<string, string> Fields { get; set; } = new();
+        
         private static bool IsValidMessage(string message)
         {
             var crc = new Crc("CRC-16/DSMR", 16, 0x8005, 0x0000, true, true, 0x0, 0x4B37);
